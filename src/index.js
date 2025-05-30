c8cb3270ac9483249a4751eab488ee7559aa7d2c
drago-develops <ambro.rot@gmail.com>
2025-05-30 20:19:07 +0100

    feat[class to create toDoS] feat[function to change obj completion]
    wrote class in toDoTasks.js that will create objects toDoS
    that later going to be dinamically added to the app.
    
    function that once called will change complition status of
    the toDoS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css"
+import { toDo, changeToDoCompletion } from "./toDoTasks.js"
+
+const taks1 = new toDo('some task', 'have to complete sometask', '02/05/25')
+const taks2 = new toDo('new task', 'new task to complete', '03/05/25')
+//taks1.changeToDoCompletion();
+console.log(taks1.complete)
+// 
+changeToDoCompletion(taks1)
+console.log(taks1)
+changeToDoCompletion(taks1)
+console.log(taks1.complete)
+console.log(taks2)
--- src/toDoTasks.js
+++ src/toDoTasks.js
@@ -1,2 +1,21 @@
 //script to create toDoTask and all their functionalities.
 
+class toDo {
+    constructor(title, descritpion, dueDate, complete) {
+        this.title = title;
+        this.descritpion = descritpion;
+        this.dueDate = dueDate;
+        this.complete = false;
+    }
+    
+}
+
+function changeToDoCompletion(obj){
+    return obj.complete == false ? obj.complete = true : obj.complete = false; 
+}
+
+
+
+
+export { toDo , changeToDoCompletion }
+
