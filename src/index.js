399d9f514dcb568bf855902a8ae76c6461a8230d
drago-develops <ambro.rot@gmail.com>
2025-05-30 20:45:20 +0100

    feat[function to change obj toDo priority]
    added function that will change toDo priority level
    between low, medium and high once user presses the button on
    the app.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import "./styles.css"
-import { toDo, changeToDoCompletion } from "./toDoTasks.js"
+import { toDo, changeToDoCompletion, priorityChange } from "./toDoTasks.js"
 
-const taks1 = new toDo('some task', 'have to complete sometask', '02/05/25')
-const taks2 = new toDo('new task', 'new task to complete', '03/05/25')
-//taks1.changeToDoCompletion();
-console.log(taks1.complete)
+const task1 = new toDo('some task', 'have to complete sometask', '02/05/25')
+const task2 = new toDo('new task', 'new task to complete', '03/05/25')
+//task1.changeToDoCompletion();
+console.log(task1.complete)
 // 
-changeToDoCompletion(taks1)
-console.log(taks1)
-changeToDoCompletion(taks1)
-console.log(taks1.complete)
-console.log(taks2)
+changeToDoCompletion(task1)
+console.log(task1)
+changeToDoCompletion(task1)
+console.log(task1.complete)
+console.log(task2)
+
+priorityChange(task2)
+console.log(task2)
+priorityChange(task2)
+console.log(task2)
+priorityChange(task2)
+console.log(task2)
--- src/toDoTasks.js
+++ src/toDoTasks.js
@@ -1,11 +1,13 @@
 //script to create toDoTask and all their functionalities.
 
 class toDo {
-    constructor(title, descritpion, dueDate, complete) {
+    constructor(title, descritpion, dueDate, priority , complete) {
         this.title = title;
         this.descritpion = descritpion;
         this.dueDate = dueDate;
+        this.priority = "low";
         this.complete = false;
+
     }
     
 }
@@ -14,8 +16,15 @@ function changeToDoCompletion(obj){
     return obj.complete == false ? obj.complete = true : obj.complete = false; 
 }
 
+function priorityChange(obj){
+    return obj.priority == "low" ? obj.priority = "medium" :
+            obj.priority == "medium" ? obj.priority = "high" :
+            obj.priority == "high" ? obj.priority = "low" :
+            "low" ;
+}
 
 
 
-export { toDo , changeToDoCompletion }
+
+export { toDo , changeToDoCompletion, priorityChange }
 
