d4a29866a4ae0f5f857410f0fa3bcfbb5a31847e
drago-develops <ambro.rot@gmail.com>
2025-06-27 20:38:17 +0100

    feat[using dialog to add projects and tasks]
    Using JS nodes to create two different forms for the
    Users to fill in the details about either task or a
    project. They will use either button to fill in a
    form about either.

--- src/dialog.js
+++ src/dialog.js
@@ -33,19 +33,30 @@ const dialogCreation = function(){
     options.setAttribute('id', 'options');
     dialogContainer.appendChild(options);
 
+    const dialogDisplay = document.createElement('div');
+    dialogDisplay.setAttribute('id', 'dialogDisplay');
+    dialogContainer.appendChild(dialogDisplay);
+
+    const dialogMessage = document.createElement('h1');
+    dialogMessage.textContent = 'Please click Task or Project button.';
+    dialogDisplay.appendChild(dialogMessage);
+
     const optionToDo = document.createElement('button');
     optionToDo.setAttribute('id', 'addToDoButton');
     optionToDo.textContent = 'Task'
     options.appendChild(optionToDo);
+    optionToDo.addEventListener('click', () => {
+      dialogTaskDisplay();
+    })
+
 
     const optionProject = document.createElement('button');
     optionProject.setAttribute('id', 'addProjectButton');
     optionProject.textContent = 'Project'
     options.appendChild(optionProject);
-
-    const dialogDisplay = document.createElement('div');
-    dialogDisplay.setAttribute('id', 'dialogDisplay');
-    dialogContainer.appendChild(dialogDisplay);
+    optionProject.addEventListener('click', () =>{
+      dialogProjcetDisplay();
+    })
 
     const formToDo = document.createElement('form')
     formToDo.setAttribute('id', 'toDoFrom');
@@ -56,6 +67,110 @@ const dialogCreation = function(){
         dialogForm.close();
     } )
     
+}
+
+//function to set multiple attributes
+function setAttributes(el, attrs) {
+  for(var key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+}
+
+//creating task form in a dialog
+const dialogTaskDisplay = function(){
+  const dialogDisplay = document.getElementById('dialogDisplay');
+  const toBeReplaced = dialogDisplay.firstChild
+
+  const form = document.createElement('form');
+  form.setAttribute('method', 'dialog');
+  toBeReplaced.replaceWith(form);
+
+  const taskFieldset = document.createElement('fieldset');
+  taskFieldset.setAttribute('id', 'taskFieldset');
+  form.appendChild(taskFieldset);
+
+  //get title of task
+  const taskTitleLabel = document.createElement('label');
+  taskTitleLabel.setAttribute('for', 'getTaskTitle');
+  taskTitleLabel.textContent = 'Title: ';
+  taskFieldset.appendChild(taskTitleLabel);
+
+  const taskTitle = document.createElement('input');
+  setAttributes(taskTitle, {'type':'input', 'id':'getTaskTitle', 'name':'taskTitle'});
+  taskFieldset.appendChild(taskTitle);
+
+  //get brief description for task 
+  const taskDescriptionLabel = document.createElement('label');
+  taskDescriptionLabel.setAttribute('for','getTaskDescription');
+  taskDescriptionLabel.textContent = 'Description: ';
+  taskFieldset.appendChild(taskDescriptionLabel);
+
+  const taskDescription = document.createElement('textarea');
+  setAttributes(taskDescription, {'id':'getTaskDescription', 'name':'taskDescription', 'maxlength':'100'});
+  taskFieldset.appendChild(taskDescription);
+
+  //set dueDate
+  const taskDueDateLabel = document.createElement('label');
+  taskDueDateLabel.setAttribute('for', 'getTaskDueDate');
+  taskDueDateLabel.textContent = 'Due date:';
+  taskFieldset.appendChild(taskDueDateLabel);
+
+  const taskDueDate = document.createElement('input');
+  setAttributes(taskDueDate, {'type':'date', 'id':'getTaskDueDate', 'name':'taskDueDate'});
+  taskFieldset.appendChild(taskDueDate);
+
+  //priority selection for a task
+  const divPriorities = document.createElement('div');
+  divPriorities.setAttribute('id', 'priorities');
+  taskFieldset.appendChild(divPriorities);
+
+  const lowPriority = document.createElement('input');
+  setAttributes(lowPriority, {'type':'radio', 'id':'low', 'name':'priority', 'value':'low', 'checked':''});
+  divPriorities.appendChild(lowPriority);
+
+  const lowPriorityLabel = document.createElement('label');
+  lowPriorityLabel.setAttribute('for', 'low');
+  lowPriorityLabel.textContent = 'Low';
+  divPriorities.appendChild(lowPriorityLabel);
+
+  const mediumPriority = document.createElement('input');
+  setAttributes(mediumPriority, {'type':'radio', 'id':'medium', 'name':'priority', 'value':'medium'});
+  divPriorities.appendChild(mediumPriority);
+
+  const mediumPriorityLabel = document.createElement('label');
+  mediumPriorityLabel.setAttribute('for', 'low');
+  mediumPriorityLabel.textContent = 'Medium'
+  divPriorities.appendChild(mediumPriorityLabel);
+
+  const highPriority = document.createElement('input');
+  setAttributes(highPriority, {'type':'radio', 'id':'high', 'name':'priority', 'value':'high'});
+  divPriorities.appendChild(highPriority);
+
+  const highPriorityLabel = document.createElement('label');
+  highPriorityLabel.setAttribute('for', 'low');
+  highPriorityLabel.textContent = 'High';
+  divPriorities.appendChild(highPriorityLabel);
+
+};  
+
+//creating project form in a dialog
+const dialogProjcetDisplay = function(){
+  const dialogDisplay = document.getElementById('dialogDisplay');
+  const toBeReplaced = dialogDisplay.firstChild
+
+  const projectFieldset = document.createElement('fieldset');
+  projectFieldset.setAttribute('id', 'projectFieldset');
+  toBeReplaced.replaceWith(projectFieldset);
+
+  //get title of project
+  const projectTitleLabel = document.createElement('label');
+  projectTitleLabel.setAttribute('for', 'getProjectTitle');
+  projectTitleLabel.textContent = 'Project Title';
+  projectFieldset.appendChild(projectTitleLabel);
+
+  const projectTitle = document.createElement('input');
+  setAttributes(projectTitle, {'type':'input', 'id':'getProjectTitle', 'name':'projectTitle'});
+  projectFieldset.appendChild(projectTitle);
 }
 
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import "./stylesDialog.css"
 import { toDo, changeToDoCompletion, priorityChange, editDescription} from "./toDoTasks.js"
 import { project } from "./projects.js"
 import { sidebarMenu } from "./sidebar.js"
-import { dialogCreation } from "./dialog.js"
+import { dialogCreation, dialogFormDisplay} from "./dialog.js"
 
 sidebarMenu();
 dialogCreation();
+//dialogFormDisplay();
+
 
 // const addButton = document.getElementById('addButton');
 // addButton.addEventListener('click', () =>{
