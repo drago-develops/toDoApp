28862e53eed791ca96db238185a740d8910abc01
drago-develops <ambro.rot@gmail.com>
2025-07-29 20:53:49 +0100

    feat[created mainDiv.js]
    created the js file that will handle populating
    mainDiv screen with task and projects.

---
+++ mainDiv.js
@@ -0,0 +1,18 @@
+import { projectArray } from "./projects.js"
+
+const homeDisplay = document.querySelector('.allToDos');
+homeDisplay.addEventListener('click', () =>{
+    taskMainDivPopulate();
+})
+
+//function to populate mainDiv
+const taskMainDivPopulate = function(){
+
+}
+
+//function that will get details of task from projects (project Arrays) 
+// and then will be used to fill in the details in taskMainDivPopulate()
+const getTaskDetails = function(){
+    
+
+}
