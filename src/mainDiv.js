1f24a6f2f54b7fdc8faf54274f38ca494365619a
drago-develops <ambro.rot@gmail.com>
2025-07-31 18:24:02 +0100

    fix[deleted mainDiv.js file]
    the original file has been created in main folder
    of app on my computer rather than the src directory.
    This caused an issue as I could find that file in
    devtools.
    same file has been recreateed from scratch in right
    place.

--- src/mainDiv.js
+++ src/mainDiv.js
@@ -1,8 +1,9 @@
-import { projectArray } from "./projects.js"
+import { myArrayModule } from "./projects.js"
 
 const homeDisplay = document.querySelector('.allToDos');
 homeDisplay.addEventListener('click', () =>{
     taskMainDivPopulate();
+    getTaskDetails();
 })
 
 //function to populate mainDiv
@@ -13,6 +14,9 @@ const taskMainDivPopulate = function(){
 //function that will get details of task from projects (project Arrays) 
 // and then will be used to fill in the details in taskMainDivPopulate()
 const getTaskDetails = function(){
+    const getProjectsInfo = myArrayModule.getArray();
     
-
+    for (const i  in getProjectsInfo){
+        console.log(getProjectsInfo[i].projectArray)
+    }
 }
